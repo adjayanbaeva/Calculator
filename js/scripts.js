7a12c92cc4407c7b9d93e22c1cc27a465736d7d4
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2018-01-20 13:18:19 -0800

    updates bmi calculator

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,7 @@
-var add = function(number1, number2) {
-  return number1 + number2;
-};
-
-var number1 = parseInt(prompt("Enter your weight in pounds:"));
-var number2 = parseInt(prompt("Enter your height in inches:"));
-var result = add(number1, number2) {
-  return number1 + number2;
-};
-
-var bmiCalculator = function(height, weight) {
-var bmi1 = multiply(height, height);
-var finalBmi = divide(weight, bmi1);
-return finalBmi;
-
-
-var divide = function(number1, number2) {
-  return number1 / number2;
-};
-
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = divide(number1, number2);
-alert(result);
-
-var multiply = function(number1, number2) {
-	return number1 * number2;
-};
-
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number"));
-var result = multiply(number1, number2);
-alert(result);
+var height = parseInt(prompt("Please enter your height"));
+var weight = parseInt(prompt("Please enter your weight"));
+var heightFinal = height * height;
+var bmiCalculator = function(heightFinal, weight){
+  return weight / heightFinal;
+}
+alert(bmiCalculator(heightFinal,weight))
